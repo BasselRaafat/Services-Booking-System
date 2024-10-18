a2c562d78ba1c68699aaef4299aa3899c39dfec8
Bassel Raafat <basselr11@gmail.com>
2024-10-18 16:23:14 +0300

    something

--- SBS.PL/Controllers/ServiceController.cs
+++ SBS.PL/Controllers/ServiceController.cs
@@ -27,12 +27,14 @@ namespace Services_Booking_System.Controllers
             if (!ModelState.IsValid)
             {
                 return View(ModelState);
+
             }
             var serviceDb = context.Service.FirstOrDefault(x => x.Id == ServiceId);
             var technicianDb = context.Technician.Where(t => t.TechnicianService.Any(ts => ts.ServiceId == ServiceId)).ToList();
             if (serviceDb == null || technicianDb == null)
             {
                 return View(ModelState);
+
             }
             var serviceProviders = new List<ServiceProviderViewModel>();
             foreach (var tech in technicianDb)
